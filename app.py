5e88dcc2398e38f3db2b58f83785510f038df0c7
gabsemus <88344550+gabsemus@users.noreply.github.com>
2022-12-12 10:45:51 -0300

    Commit

--- app.py
+++ app.py
@@ -4,125 +4,49 @@ from db_fxns import *
 import streamlit.components.v1 as stc
 
 
-
-# Data Viz Pkgs
-import plotly.express as px 
-
-
 HTML_BANNER = """
     <div style="background-color:#464e5f;padding:10px;border-radius:10px">
-    <h1 style="color:white;text-align:center;">ToDo App (CRUD)</h1>
-    <p style="color:white;text-align:center;">Built with Streamlit</p>
+    <h1 style="color:white;text-align:center;">Planejando o Rolê</h1>
+    <p style="color:white;text-align:center;">Gato e Gata em São Paulo</p>
     </div>
     """
-
 
 def main():
 	stc.html(HTML_BANNER)
 
 
-	menu = ["Create","Read","Update","Delete","About"]
+	menu = ["Inserir","Visualizar","Excluir"]
 	choice = st.sidebar.selectbox("Menu",menu)
 	create_table()
 
-	if choice == "Create":
-		st.subheader("Add Item")
-		col1,col2 = st.beta_columns(2)
-		
-		with col1:
-			task = st.text_area("Task To Do")
+	if choice == "Inserir":
+		st.subheader("Adicionar a Lista: ")
 
-		with col2:
-			task_status = st.selectbox("Status",["ToDo","Doing","Done"])
-			task_due_date = st.date_input("Due Date")
+		nome = st.text_input("Não esquecer de: ")
 
-		if st.button("Add Task"):
-			add_data(task,task_status,task_due_date)
-			st.success("Added ::{} ::To Task".format(task))
+		quantidade = st.number_input("Quantidade: ", step = 1)
 
+		motivo = st.text_area("Observação (opcional) : ")
 
-	elif choice == "Read":
-		# st.subheader("View Items")
-		with st.beta_expander("View All"):
-			result = view_all_data()
-			# st.write(result)
-			clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
-			st.dataframe(clean_df)
+		if st.button("Adicionar"):
+			inserir_pendencia(nome,quantidade,motivo)
+			st.success("Boa seu noia!")
 
-		with st.beta_expander("Task Status"):
-			task_df = clean_df['Status'].value_counts().to_frame()
-			# st.dataframe(task_df)
-			task_df = task_df.reset_index()
-			st.dataframe(task_df)
+	elif choice == "Visualizar":
+		st.subheader("Visualizar Pendências")
+		result = visualizar_todos()
+		# st.write(result)
+		df = pd.DataFrame(result,columns=["Pendência","Quantidade","Motivo"])
+		st.dataframe(df)
 
-			p1 = px.pie(task_df,names='index',values='Status')
-			st.plotly_chart(p1,use_container_width=True)
+	elif choice == "Excluir":
+		st.subheader("Excluir")
 
-
-	elif choice == "Update":
-		st.subheader("Edit Items")
-		with st.beta_expander("Current Data"):
-			result = view_all_data()
-			# st.write(result)
-			clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
-			st.dataframe(clean_df)
-
-		list_of_tasks = [i[0] for i in view_all_task_names()]
-		selected_task = st.selectbox("Task",list_of_tasks)
-		task_result = get_task(selected_task)
-		# st.write(task_result)
-
-		if task_result:
-			task = task_result[0][0]
-			task_status = task_result[0][1]
-			task_due_date = task_result[0][2]
-
-			col1,col2 = st.beta_columns(2)
-			
-			with col1:
-				new_task = st.text_area("Task To Do",task)
-
-			with col2:
-				new_task_status = st.selectbox(task_status,["ToDo","Doing","Done"])
-				new_task_due_date = st.date_input(task_due_date)
-
-			if st.button("Update Task"):
-				edit_task_data(new_task,new_task_status,new_task_due_date,task,task_status,task_due_date)
-				st.success("Updated ::{} ::To {}".format(task,new_task))
-
-			with st.beta_expander("View Updated Data"):
-				result = view_all_data()
-				# st.write(result)
-				clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
-				st.dataframe(clean_df)
-
-
-	elif choice == "Delete":
-		st.subheader("Delete")
-		with st.beta_expander("View Data"):
-			result = view_all_data()
-			# st.write(result)
-			clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
-			st.dataframe(clean_df)
-
-		unique_list = [i[0] for i in view_all_task_names()]
-		delete_by_task_name =  st.selectbox("Select Task",unique_list)
-		if st.button("Delete"):
-			delete_data(delete_by_task_name)
-			st.warning("Deleted: '{}'".format(delete_by_task_name))
-
-		with st.beta_expander("Updated Data"):
-			result = view_all_data()
-			# st.write(result)
-			clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
-			st.dataframe(clean_df)
-
-	else:
-		st.subheader("About ToDo List App")
-		st.info("Built with Streamlit")
-		st.info("Jesus Saves @JCharisTech")
-		st.text("Jesse E.Agbe(JCharis)")
-
+		lista_pendencias = [i[0] for i in pendencias_por_nome()]
+		pendencia_nome =  st.selectbox("Selecione a Pendência",lista_pendencias)
+		if st.button("Excluir"):
+			excluir_pendencia(pendencia_nome)
+			st.warning("Excluido: '{}'".format(pendencia_nome))
 
 if __name__ == '__main__':
 	main()
--- db_fxns.py
+++ db_fxns.py
@@ -4,40 +4,22 @@ c = conn.cursor()
 
 
 def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS taskstable(task TEXT,task_status TEXT,task_due_date DATE)')
+	c.execute('CREATE TABLE IF NOT EXISTS pendencia(nome TEXT,quantidade TEXT,motivo TEXT)')
 
-
-def add_data(task,task_status,task_due_date):
-	c.execute('INSERT INTO taskstable(task,task_status,task_due_date) VALUES (?,?,?)',(task,task_status,task_due_date))
+def inserir_pendencia(nome,quantidade,motivo):
+	c.execute('INSERT INTO pendencia(nome,quantidade,motivo) VALUES (?,?,?)',(nome,quantidade,motivo))
 	conn.commit()
 
-
-def view_all_data():
-	c.execute('SELECT * FROM taskstable')
+def visualizar_todos():
+	c.execute('SELECT * FROM pendencia')
 	data = c.fetchall()
 	return data
 
-def view_all_task_names():
-	c.execute('SELECT DISTINCT task FROM taskstable')
+def pendencias_por_nome():
+	c.execute('SELECT DISTINCT nome FROM pendencia')
 	data = c.fetchall()
 	return data
 
-def get_task(task):
-	c.execute('SELECT * FROM taskstable WHERE task="{}"'.format(task))
-	data = c.fetchall()
-	return data
-
-def get_task_by_status(task_status):
-	c.execute('SELECT * FROM taskstable WHERE task_status="{}"'.format(task_status))
-	data = c.fetchall()
-
-
-def edit_task_data(new_task,new_task_status,new_task_date,task,task_status,task_due_date):
-	c.execute("UPDATE taskstable SET task =?,task_status=?,task_due_date=? WHERE task=? and task_status=? and task_due_date=? ",(new_task,new_task_status,new_task_date,task,task_status,task_due_date))
-	conn.commit()
-	data = c.fetchall()
-	return data
-
-def delete_data(task):
-	c.execute('DELETE FROM taskstable WHERE task="{}"'.format(task))
+def excluir_pendencia(nome):
+	c.execute('DELETE FROM pendencia WHERE nome="{}"'.format(nome))
 	conn.commit()
